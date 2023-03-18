a8a31c038bdbafae17c085ff6663c5a8b9bb742e
Dimasstep <fanzor2006@gmail.com>
2023-03-18 12:24:39 +0300

    Добавили скрипт

---
+++ demo.js
@@ -0,0 +1,7 @@
+// alert('Приветствую, Вы зашли на тестовый сайт');{
+//     prompt('Как Вас зовут?');
+// }
+
+// document.write('Добро пожаловать на сайт');
+
+ const buttons = ducument.getElementsDyClassName('modal-button');
